b49b13caab42362614477ed85523c40b96a1ba4c
Renan Marcel Uchôa <renanmarceluchoa@gmail.com>
2013-01-30 21:08:19 -0200

    comitando o singleton

--- sistema-carona-rp/src/java/jdbc/ConnectionFactory.java
+++ sistema-carona-rp/src/java/jdbc/ConnectionFactory.java
@@ -15,12 +15,21 @@ import java.sql.SQLException;
  * @author Usuario
  */
 public class ConnectionFactory {
-    public Connection getConnection() throws SQLException {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://localhost/frota", "root", "");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new SQLException(e);
+    
+    private static Connection connection;
+    
+    public static Connection getConnection() throws SQLException {
+        
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/frota", "root", "");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new SQLException(e);
+            } 
         }
+        
+        return connection;
+        
     }
 }
